986b737a5b5f7ba6089f8a30fca058761e6afb31
hrishigrit <83822397+hrishigrit@users.noreply.github.com>
2021-05-11 13:11:06 +0530

    Add files via upload

---
+++ MyCap-Task1.py
@@ -0,0 +1,9 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> radius=float(input("Enter the radius of the circle: "))
+Enter the radius of the circle: 1.1
+>>> pi= 3.141592653589793238
+>>> pi*radius**2
+3.8013271108436504
+>>> Area=input("Area of circle with radius 1.1 is : ")
+Area of circle with radius 1.1 is : 3.8013271108436504
